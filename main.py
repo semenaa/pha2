f70ecaf09aa9400d3607b97aa3f964218940a1d9
semenaa <semenaa@yandex.ru>
2023-01-17 19:16:30 +0500

    done

--- main.py
+++ main.py
@@ -7,11 +7,10 @@ if __name__ == '__main__':
         contacts_list = list(rows)
     # pprint(contacts_list)
 
-    # TODO 1: выполните пункты 1-3 ДЗ
+    # Набить второй список из "исправленных" строк
     new_contacts_list = [contacts_list[0]]
     row = 0
     for contact in contacts_list:
-        row += 1
         new_contacts_list += [[''] * 7]
         # Разобрать ФИО на поля
         names = re.findall('[а-я]+', contact[0], flags=re.I)
@@ -26,39 +25,45 @@ if __name__ == '__main__':
                           contact[5])
         if phone is not None:
             new_contacts_list[row][5] = phone.group(1) + '(' + phone.group(2) + ')' + \
-            phone.group(3) + '-' + phone.group(4) + '-' + phone.group(5)
+                phone.group(3) + '-' + phone.group(4) + '-' + phone.group(5)
             # добавочные номера
             if phone.group(6) is not None:
                 new_contacts_list[row][5] += ' ' + phone.group(6) + phone.group(7)
 
         # Скопировать организацию и e-mail
-        new_contacts_list[row - 1][3] = contacts_list[row - 1][3]
-        new_contacts_list[row - 1][4] = contacts_list[row - 1][4]
-        new_contacts_list[row - 1][6] = contacts_list[row - 1][6]
-
+        new_contacts_list[row][3] = contacts_list[row][3]
+        new_contacts_list[row][4] = contacts_list[row][4]
+        new_contacts_list[row][6] = contacts_list[row][6]
+        row += 1
+    new_contacts_list.pop(-1)
 
     def merge(contact, another_contact):
         counter = 0
         for col in contact:
             if col == '':
-                col += another_contact[counter]
+                contact[counter] = another_contact[counter]
+            counter += 1
 
 
-
-    # Слить дублирующиеся записи
+    # Слить дублирующиеся записи, отметить лишние
     for contact in new_contacts_list:
         for another_contact in new_contacts_list:
+            if another_contact == contact:
+                continue
             counter = 0
             if contact[0] == another_contact[0]:
                 if contact[1] == another_contact[1] or another_contact[1] == '':
                     if contact[2] == another_contact[2] or another_contact[2] == '':
                         merge(contact, another_contact)
+                        another_contact[0] = '_delete_'
             counter += 1
+    # Удалить лишние записи
+    for contact in new_contacts_list:
+        if contact[0] == '_delete_':
+            new_contacts_list.remove(contact)
 
-
-    # TODO 2: сохраните получившиеся данные в другой файл
     # код для записи файла в формате CSV
-    with open("phonebook.csv", "w", encoding='utf-8') as f:
+    with open("phonebook.csv", "w", encoding='utf-8', newline='') as f:
         datawriter = csv.writer(f, delimiter=',')
         # Вместо contacts_list подставьте свой список
         datawriter.writerows(new_contacts_list)
